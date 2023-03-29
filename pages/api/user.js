8b9238f85c3c02bb2ea08a33d1cb825307d98049
ryanl09 <71403615+ryanl09@users.noreply.github.com>
2023-03-29 15:52:28 -0400

    convos

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,29 +1,110 @@
 import query from '../../lib/db';
 
 export default async function handler(req, res){
-    res.status(200).json({success:'hu'})
-    return;
-    if (req.method === 'GET'){
-        const { action } = req.query;
 
-        if(action===undefined){
-            res.status(200).json({error:'Invalid action'});
+    if (req.method === 'POST'){
+
+        const { action } = req.body;
+    
+        if (action===undefined){
+            res.status(200).json({error:'Invalid request'});
+            return;
+        }
+        
+        const { userId } = req.body;
+        if(userId === undefined){
+            res.status(200).json({error:'Invalid user'});
+            return;
+        }
+    
+        switch(action){
+            case 'sendMessage':
+                const { message, userTo } = req.body;
+                const sentStatus = await sendMessage(userId, userTo, message);
+                res.status(200).json(sentStatus);
+                break;
+            case 'deleteMessage':
+                const { messageId } = req.body;
+                break;
+            case 'requestAction':
+                const { requestId, status } = req.body;
+
+                break;
+        }
+    } else if (req.method === 'GET'){
+        const {action } = req.query;
+        if (action===undefined){
+            res.status(200).json({error:'Invalid request'});
             return;
         }
 
-        const { username } = req.query;
-        if (username == undefined){
+        const { userId } = req.query;
+        if(userId === undefined){
             res.status(200).json({error:'Invalid user'});
             return;
         }
 
-        const userId = await getUserId(username);
-
-        switch (action) {
-            case 'getUserInfo':
-                const info = await getUserInfo(userId);
-                res.status(200).json(info);
+    
+        switch(action){
+            case 'getMessages':
+                break;
+            case 'getConversations':
+                const convos = await getConversations(userId);
+                break;
+            case 'getRequests':
                 break;
         }
     }
+}
+
+async function sendMessage(userId, userTo, message){
+    const sql ='INSERT INTO `messages` (`userFrom`, `userTo`, `msg`) VALUES (?, ?, ?)';
+    const vals = [userId, userTo, message];
+    
+    const result = await query(sql ,vals);
+
+    if(result.error){
+        return result;
+    }
+
+    return{
+        sent:true
+    };
+}
+
+async function getMessages(userId, conversationId) {
+
+}
+
+async function createConversation(users){
+    const sql='INSERT INTO `conver';
+    const vals = [...users];
+}
+
+async function getConversations(userId){
+    const sql='SELECT DISTINCT users.username FROM messages m INNER JOIN users ON users.id = m.userTo WHERE m.userFrom = ?';
+    const vals=[userId];
+
+    const result = await query(sql, vals);
+    if(result.error){
+        return result;
+    }
+
+    const res = [];
+    result.forEach(e=>{
+        res.push({
+            username: e.username,
+            image: 'https://api.tecesports.com/images/general/user.png'
+        });
+    });
+
+    return res;
+}
+
+async function deleteMessage(userId, messageId){
+    sql='';
+}
+
+async function getRequests(userId){
+
 }
--- pages/user/[username].js
+++ pages/user/[username].js
@@ -2,11 +2,33 @@ import React from 'react';
 import Content from '@/components/Content';
 import axios from 'axios';
 import query from '@/lib/db';
+import Image from 'next/image';
+import Router, { useRouter } from 'next/router';
 
 export default function User({info}){
 
+    const router = useRouter();
+    let { username } = router.query;
+    if(username===undefined){
+        username='ryan';
+    }
+    
+
     return (
-        <Content></Content>
+        <Content>
+            <div className='flex items-center'>
+                <div>
+                    <div className='max-w-[80px] max-h-[80px]'>
+                        <Image src={info.image ?? 'https://api.tecesports.com/images/general/user.png'} 
+                        className='rounded-full' width="80" height="80" alt="User" />
+                    </div>
+                    <div className='flex flex-col gap-1'>
+                        <h1>{username}</h1>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+        </Content>
     );
 }
 
@@ -27,8 +49,8 @@ export async function getServerSideProps(context){
     }
 
     async function getUserInfo(userId){
-        const accountTags = getAccountTags(userId);
-        const musicTags = getMusicTags(userId);
+        const accountTags = await getAccountTags(userId);
+        const musicTags = await getMusicTags(userId);
 
         return {
             image: 'https://api.tecesports.com/images/general/user.png',
@@ -42,8 +64,9 @@ export async function getServerSideProps(context){
     async function getAccountTags(userId){
         const sql = `SELECT accountTags.tagName
         FROM userAccountTags
-        INNER JOIN accountTags ON accountTags.id = userAccountTags.tagId
-        WHERE userAccountTags.userId = ?`;
+        INNER JOIN accountTags ON accountTags.id = userAccountTags.accountTagId
+        WHERE userAccountTags.userId = ?
+        ORDER BY accountTags.tagName`;
 
         const vals = [parseInt(userId, 10)];
         const result = await query(sql, vals);
@@ -51,14 +74,20 @@ export async function getServerSideProps(context){
             return result;
         }
 
-        return [];
+        const res = [];
+        result.forEach(e=>{
+            res.push(e.tagName);
+        })
+
+        return res;
     }
 
     async function getMusicTags(userId){
         const sql = `SELECT musicTags.tagName
         FROM userMusicTags
-        INNER JOIN musicTags ON accountTags.id = userMusicTags.tagId
-        WHERE userMusicTags.userId = ?`;
+        INNER JOIN musicTags ON musicTags.id = userMusicTags.musicTagId
+        WHERE userMusicTags.userId = ?
+        ORDER BY musicTags.tagName`;
 
         const vals = [parseInt(userId, 10)];
         const result = await query(sql, vals);
@@ -66,16 +95,19 @@ export async function getServerSideProps(context){
             return result;
         }
 
-        return [];
+        const res=[];
+        result.forEach(e =>{
+            res.push(e.tagName);
+        });
+
+        return res;
     }
 
     if (username === undefined){
         username = 'ryan';
     }
     const userId = await getUserId(username);
-
-    console.log(userId);
-    const info = await getUserInfo(userId);
+    const info = await getUserInfo(userId.id);
 
     console.log(info);
 
